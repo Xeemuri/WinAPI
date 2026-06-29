40f801e7706a957ab9a0850000e711f11ff44355
Илья Щербаков <iliya309@mail.ru>
2026-06-29 19:33:37 +1000

    Add project files.

---
+++ DialogBox/main.cpp
@@ -0,0 +1,25 @@
+#include <Windows.h>
+#include "resource.h"
+
+//Прототип функции:
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, NULL);
+}
+//Рализация функции:
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
+}
---
+++ WinAPI/main.cpp
@@ -0,0 +1,13 @@
+﻿#include<Windows.h>
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	MessageBox
+	(
+		NULL, //Родительское окно
+		"Hello WinAPI!\nРазработка настольных приложений для Windows. ", //Сообщение, выводимое пользователюз
+		"Hello", //Заголовок окна
+		MB_YESNOCANCEL | MB_HELP | MB_ICONASTERISK | MB_DEFBUTTON3 | MB_SYSTEMMODAL//Флаги, которые определяют значок, выводимый в окне, набор кнопок и некоторые дополнительные параметры
+	);
+
+}
