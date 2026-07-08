476a9020b3ba89359045b59a6fe8e4f954f85342
Илья Щербаков <iliya309@mail.ru>
2026-07-08 18:24:13 +1000

    Login invitation DONE

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -1,5 +1,8 @@
 #include <Windows.h>
 #include "resource.h"
+#include <time.h>
+
+CONST CHAR g_sz_INVITE[] = "Введите имя пользователя";
 
 //Прототип функции:
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -17,29 +20,42 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+
+		SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);
+
 	}
-		break;
+	break;
 	case WM_COMMAND:	//Обработка кнопок, действий мышью и т.д.
+	{
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+		CONST INT SIZE = 256;
+		CHAR szBuffer[SIZE] = {};
 		switch (LOWORD(wParam))
 		{
+		case IDC_EDIT_LOGIN:
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)szBuffer);
+			if (HIWORD(wParam) == EN_SETFOCUS && strcmp(szBuffer, g_sz_INVITE) == 0)
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
+			if (HIWORD(wParam) == EN_KILLFOCUS && strcmp(szBuffer, "") == 0)
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);
+			break;
 		case IDC_BUTTON_COPY:
 		{
-			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
-			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
-			CONST INT SIZE = 256;
-			CHAR szBuffer[SIZE] = {};
 			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)szBuffer);
 			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)szBuffer);
 		}
-		
-			break;
+		break;
 		case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК!", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
-		break;
+	}
+	break;
+
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
 		break;
 	}
+	
 	return FALSE;
 }
